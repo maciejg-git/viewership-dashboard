261ac1fe7689c01afad677679e7b4b5bcbb7493f
ender <ender@localhost>
2020-11-03 14:30:44 +0100

    refactor tabs to spaces for better github display

--- src/router/index.js
+++ src/router/index.js
@@ -4,21 +4,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+  const routes = []
 
 const router = new VueRouter({
   mode: 'history',
